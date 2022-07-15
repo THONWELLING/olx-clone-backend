a43516d732e582dd5d3749afda3d28f758e61553
THONWELLING <thonwellingdani13@gmail.com>
2022-07-14 23:37:41 -0300

    Criado a função para pegar as categorias e add url de imagem

--- src/controllers/AdsController.ts
+++ src/controllers/AdsController.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from "express";
 
+
+const Category = require('../models/Category')
+
+
 export const AdsController = {
   getCategories: async(req: Request, res: Response) => {
+    const cats = await Category.find()
 
+    let categories =[]
+    for(let i in cats) {
+      categories.push({
+        ...cats[i],
+        img: `${process.env.BASE}/assets/images/${cats[i].slug}.png`
+      })
+    }
+
+    res.json({ categories })
   },
 
   addAction: async(req: Request, res: Response) => {
